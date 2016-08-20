8a8cfe2343ae4be8e1ddb9e8a914799879e13d32
Alex Nitta <a.j.nitta@gmail.com>
2016-08-20 07:28:47 -0700

    Edit server.js

--- server/server.js
+++ server/server.js
@@ -33,27 +33,23 @@ uncomment the following lines once you have set up the certificate files
 and deleted or commented the lines above
 *************/
 
-/* 
-
-// set up path to key and certificate files
+// // set up path to key and certificate files
  
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/www.node-https-example.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/www.node-https-example.com/cert.pem')
-};
+// const options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/www.node-https-example.com/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/www.node-https-example.com/cert.pem')
+// };
  
-// create an HTTPS service 
-https.createServer(options, app).listen(443, function(err) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('node-https-example server listening on port 443. HTTPS is working!');
-});
+// // create an HTTPS service 
+// https.createServer(options, app).listen(443, function(err) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   console.log('node-https-example server listening on port 443. HTTPS is working!');
+// });
  
-// redirect from http (port 80) to https (port 443)
-http.createServer(function (req, res) {
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-  res.end();
-}).listen(80);
-
-*/
+// // redirect from http (port 80) to https (port 443)
+// http.createServer(function (req, res) {
+//   res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//   res.end();
+// }).listen(80);
