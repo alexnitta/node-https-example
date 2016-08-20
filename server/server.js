c0c2672ef61aff23f7f26f60e1a4cfa75e437110
Alex Nitta <a.j.nitta@gmail.com>
2016-08-20 07:20:31 -0700

    Edit server.js

--- server/server.js
+++ server/server.js
@@ -25,6 +25,8 @@ setting up the certificate files
 *************/
 
 http.createServer(app).listen(80);
+console.log('node-https-example server listening on port 80. Remember to' +
+  ' modify server.js after you set up letsencrypt certificates.');
 
 /************
 uncomment the following once you have set up the certificate files and 
@@ -36,8 +38,8 @@ deleted or commented the createServer line above
 // set up path to key and certificate files
  
 const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/node-https-example.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/node-https-example.com/cert.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/www.node-https-example.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.node-https-example.com/cert.pem')
 };
  
 // create an HTTPS service 
@@ -45,7 +47,7 @@ https.createServer(options, app).listen(443, function(err) {
   if (err) {
     return console.log(err);
   }
-  console.log('Server is listening on port 443');
+  console.log('node-https-example server listening on port 443. HTTPS is working!');
 });
  
 // redirect from http (port 80) to https (port 443)
